51ee7b9e1f0ea5054646978144fe062c2bcacba0
mccrush <mccrush2027@gmail.com>
2019-04-02 20:38:39 +0700

    Наполняет русский язык перевода

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@
 
     <img src="/img/icons/favicon-32x32.png" width="32" height="32" :alt="local.titleapp">
     <span class="font-roboto-slab">{{local.titleapp}}</span>
-    <i class="material-icons md-24 btn btn-sm" title="Settings" data-toggle="tooltip" data-placement="left" @click="showSettings">settings</i>
+    <i class="material-icons md-24 btn btn-sm" :title="local.settings" data-toggle="tooltip" data-placement="left" @click="showSettings">settings</i>
   </nav>
 </template>
 <script>
--- src/language/en.js
+++ src/language/en.js
@@ -19,8 +19,11 @@ export default {
     buttoncloseforeve: 'Close forever',
     buttonclose: 'I understood'
   },
-  descriptionapp: '<small><p>In psychology, the Stroop effect is a demonstration of interference in the reaction time of a task.</p><p>When the name of a color (e.g., "blue", "green", or "red") is printed in a color which is not denoted by the name (i.e., the word "red" printed in blue ink instead of red ink), naming the color of the word takes longer and is more prone to errors than when the color of the ink matches the name of the color.</p></small>',
+  descriptionapp: '<small><p>In psychology, <a href="https://www.wikiwand.com/en/Stroop_effect" target="_blank" title="Wikipedia" data-toggle="tooltip" data-placement="bottom">the Stroop effect</a> is a demonstration of interference in the reaction time of a task.</p><p>When the name of a color (e.g., "blue", "green", or "red") is printed in a color which is not denoted by the name (i.e., the word "red" printed in blue ink instead of red ink), naming the color of the word takes longer and is more prone to errors than when the color of the ink matches the name of the color.</p></small>',
   colors: {
-    red: 'Red'
+    red: 'Red',
+    blue: 'Blue',
+    yellow: 'Yellow',
+    green: 'Green'
   }
 }
