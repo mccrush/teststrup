075f0608fb6cbc72c46bfc494ac31794edd410bb
mccrush <mccrush2027@gmail.com>
2019-12-31 12:36:55 +0700

    Настраивает отображение Футера

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,24 @@
 <template>
-  <nav class="navbar fixed-bottom navbar-dark bg-dark d-flex justify-content-center pt-0 pb-0">
+  <div class="text-center bg-dark position-absolute w-100 ml-n3 p-2">
     <p class="text-muted font-weight-light mb-0">
       <small>
         Powered by&nbsp;
         <a class="text-muted under" href="https://mccrush.ru/" target="_blank">mccrush.ru</a>
-        <span class="text-white-50">&nbsp;&nbsp;&starf;&nbsp;&nbsp;</span>
+        <br />
+        <!-- <span class="text-white-50">&nbsp;&nbsp;&starf;&nbsp;&nbsp;</span> -->
         Fork me on&nbsp;
         <a class="text-muted under" href="https://github.com/mccrush/teststrup" target="_blank">GitHub</a>
       </small>
     </p>
-  </nav>
+  </div>
 </template>
 <style scoped>
 a {
   text-decoration: underline;
+}
+
+.text-center {
+  line-height: 1;
+  bottom: 0px;
 }
 </style>
