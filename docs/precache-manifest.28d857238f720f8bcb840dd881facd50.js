9927acc977635c76a76ecc87765e1b49ed34cb38
mccrush <mccrush2027@gmail.com>
2020-06-06 22:12:07 +0700

    Deploy 3.0.1

---
+++ docs/precache-manifest.28d857238f720f8bcb840dd881facd50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "500da3f718cea23b6712",
+    "url": "css/app.de3ac7da.css"
+  },
+  {
+    "revision": "313816b44858d5e61e52",
+    "url": "css/chunk-vendors.feaf6c1a.css"
+  },
+  {
+    "revision": "3ebbece0af119a65c0f4543666aa1b1d",
+    "url": "img/re.png"
+  },
+  {
+    "revision": "81eb0ccf0c4d9f65829dec2ad0139bcf",
+    "url": "index.html"
+  },
+  {
+    "revision": "500da3f718cea23b6712",
+    "url": "js/app.0f3791ee.js"
+  },
+  {
+    "revision": "313816b44858d5e61e52",
+    "url": "js/chunk-vendors.32ee0a6a.js"
+  },
+  {
+    "revision": "679e904543202dea7fc790cd76a8e1f5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6887047fdec944f75714efa206286eae.js"
+  "precache-manifest.28d857238f720f8bcb840dd881facd50.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "teststrup"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "teststrup"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
